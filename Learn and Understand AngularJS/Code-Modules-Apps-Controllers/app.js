f7872a7ad6ab9084e81bc2f2ceda382984c6c671
Luis Laija <laija11@hotmail.com>
2018-07-10 17:13:10 -0500

    10-07-2018 17:13pm

---
+++ Learn and Understand AngularJS/Code-Modules-Apps-Controllers/app.js
@@ -0,0 +1,10 @@
+// MODULE -id to connect with the DOM and assignes this to an object
+var angularApp = angular.module('angularApp', []); // it takes an array of depencencies
+
+// CONTROLLERS - controllers are available for modules, the con troler is the model en html body is the view
+angularApp.controller('mainController', ['$scope', function ($scope) {
+    
+}]);
+
+
+
