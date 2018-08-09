5255987a32233222293a4e9a762f0722b23d801f
Luis Laija <laija11@hotmail.com>
2018-08-09 17:01:03 -0500

    17:00 09-08-2018

--- Desarrollar una red social con JavaScript, Angular y NodeJS/curso-mean-social/api/Routes/user.js
+++ Desarrollar una red social con JavaScript, Angular y NodeJS/curso-mean-social/api/Routes/user.js
@@ -4,11 +4,16 @@ var express = require('express');
 var UserController = require('../controllers/user');
 
 var api = express.Router(); // para terner acceso a los metodos get, post etc
+var md_auth = require('../middlewares/authenticated');
 
 api.get('/home', UserController.home);
 
-api.get('/pruebas', UserController.pruebas);
+api.get('/pruebas', md_auth.ensureAuth, UserController.pruebas);
 
 api.post('/register', UserController.saveUser);
+
+api.post('/login', UserController.loginUser);
+
+api.get('/user/:id', md_auth.ensureAuth, UserController.getUser);
 
 module.exports = api;
--- Desarrollar una red social con JavaScript, Angular y NodeJS/curso-mean-social/api/controllers/user.js
+++ Desarrollar una red social con JavaScript, Angular y NodeJS/curso-mean-social/api/controllers/user.js
@@ -1,11 +1,11 @@
 'use strict' // ayuda a tener las funciones de flecha 
 
 var bcrypt = require('bcrypt-nodejs'); // para cifrar las contrasennas
-
+var jwt = require('../services/jwt');
 // para indicar que es un modelo la primera letra de la variable se ponen en mayuscula 
 var User = require('../models/user');
  
-
+// Metodos de prueba 
 function home(req, res) {
 	res.status(200).send({
 		message: 'hola mundo home'
@@ -18,6 +18,7 @@ function pruebas(req, res) {
 	});
 };
 
+// registro 
 function saveUser(req, res){
 	var user = new User(); // crear un objeto del modelo de usuario 
 	var params = req.body; // todo los request que lleguen por post, los vamos a recoger y guardar en esa variable 
@@ -30,18 +31,29 @@ function saveUser(req, res){
 		user.role = 'ROLE_USER';
 		user.image = null;
 
-		bcrypt.hash(params.password, null, null, (err, hash) =>{
-			user.password = hash;
-			user.save((err, userStored) =>{
-				if(err) return res.status(500).send({message: 'Error al guardar el usuario'});
-				if(userStored){
-					res.status(200).send({user: userStored});
-				}else{
-					res.status(400).send({message: 'No se ha registrado el usuario'})
+		// Controlar usuarios duplicados by cheking email and nick 
+		User.find({ $or: [{email: user.email.toLowerCase()}, {nick: user.nick.toLowerCase()}
+			]}).exec((err, users) => {
+				if(err) return res.status(500).send({message: 'Error en la peticion de usuarios'});
+
+				if(users && users.length >= 1){
+					return res.status(200).send({message: 'El usuario que intentas registrar ya existe'})
+				}
+				else{
+					// Cifra la contrasenna y guarda los datos
+					bcrypt.hash(params.password, null, null, (err, hash) =>{
+						user.password = hash;
+						user.save((err, userStored) =>{
+							if(err) return res.status(500).send({message: 'Error al guardar el usuario'});
+							if(userStored){
+								res.status(200).send({user: userStored});
+							}else{
+								res.status(400).send({message: 'No se ha registrado el usuario'})
+							}
+						});
+					});						
 				}
 			});
-
-		});
 	}else {
 		res.status(200).send({
 			message: 'Envia todos los campos necesarios'
@@ -50,9 +62,61 @@ function saveUser(req, res){
 
 }
 
+// Login
+function loginUser(req, res){
+	var params =  req.body;
+	var email = params.email;
+	var password = params.password;
+
+	// haciendo un and
+	User.findOne({email: email}, (err, user) => {
+		if(err) return res.status(500).send({message: 'Error en la peticion'});
+
+		if(user){
+			bcrypt.compare(password, user.password, (err, check) =>{
+				if(check){
+					// Devolver datos de susuario 
+					if(params.gettoken){
+						// Generar y Devolver token
+
+						return res.status(200).send({
+							token: jwt.createToken(user)
+						});
+					}else{
+						user.password = undefined;
+						return res.status(200).send({user});
+					}
+				}else{
+					return res.status(404).send({message: 'El usuario no se a podido identificar'});
+				}
+			});
+		}
+		else{
+			return res.status(404).send({message: 'El usuairo no se a podido identificar!!!'});
+		}
+	});
+
+}
+
+// datos de un usuario 
+
+function getUser(req, res){
+	// cuando llegan datos por la url utilizamos params, cuando llegan datos por post or put utilizamos body
+	var userId = req.params.id;
+	User.findById(userId, (err, user) => {
+		if(err) res.status(500).send({message: 'Error en la peticion'});
+
+		if(!user) return res.status(404).send({message: 'El usuario no existe'});
+
+		return res.status(200).send({user});
+	})
+}
+
 // se exportan en formato de objeto para luego poder hacer uso de ellas 
 module.exports = {
 	home,
 	pruebas,
-	saveUser
-}
+	saveUser,
+	loginUser,
+	getUser
+} 
