f5291f688af78beb5da7f8e0f9e8a43954dee8e5
Luis Laija <laija11@hotmail.com>
2018-08-14 17:10:45 -0500

    2018/08/14 17:10

--- Desarrollar una red social con JavaScript, Angular y NodeJS/curso-mean-social/api/controllers/follow.js
+++ Desarrollar una red social con JavaScript, Angular y NodeJS/curso-mean-social/api/controllers/follow.js
@@ -32,7 +32,70 @@ function deleteFollow(req, res){
 	});
 }
 
+function getFollowingUsers(req, res){
+	var userId = req.user.sub;
+
+	if(req.params.id && req.params.page){
+		userId = req.user.id;
+	}
+
+	var page =1; // page by default
+
+	if(req.params.page){
+		page = req.params.page;
+	}else {
+		page  = req.params.id;
+	}
+
+	var itemsPerpage = 5;
+	// encontrar a todos los usuarios que sigo 
+	Follow.find({user: followed}).populate({path: 'followed'}).paginate(page, itemsPerpage, (err, follows, total) =>{
+		if(err) return res.status(500).send({message: 'Error en el servidor'});
+
+		if(!follows) return res.status(404).send({message: 'No te sigue ningun usuario'});
+
+		return res.status(200).send({
+			total: total, 
+			pages: Math.ceil(total/itemsPerpage),
+			follows
+		})
+	});
+}
+
+function getFollowedUsers(req, res){
+	var userId = req.user.sub;
+
+	if(req.params.id && req.params.page){
+		userId = req.user.id;
+	}
+
+	var page =1; // page by default
+
+	if(req.params.page){
+		page = req.params.page;
+	}else {
+		page  = req.params.id;
+	}
+
+	var itemsPerpage = 5;
+	// encontrar a todos los usuarios que sigo 
+	Follow.find({user: userId}).populate({path: 'user'}).paginate(page, itemsPerpage, (err, follows, total) =>{
+		if(err) return res.status(500).send({message: 'Error en el servidor'});
+
+		if(!follows) return res.status(404).send({message: 'No estan siguiendo a ningun usuario'});
+
+		return res.status(200).send({
+			total: total, 
+			pages: Math.ceil(total/itemsPerpage),
+			follows
+		})
+	});	
+}
+
+
 module.exports = {
 	saveFollow,
-	deleteFollow
+	deleteFollow,
+	getFollowingUsers,
+	getFollowedUsers
 }
